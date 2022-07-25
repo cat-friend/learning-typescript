4b6e4ec74744baddcdf82c07a6e8886d0fb9f62f
cat-friend <denisepli@gmail.com>
2022-07-25 15:43:15 -0700

    used --noImplicitAny flag

--- hello.js
+++ hello.js
@@ -1,5 +1,10 @@
-console.log("Hello world!");
+// the following lines for `greet` generated an error when compiling but `tsc` still emitted code; to prevent emitting when there's an error, use the flag `--noEmitOnError` before the file name when using `tsc`
 function greet(person, date) {
     console.log("Hello ".concat(person, ", today is ").concat(date));
 }
-greet("Wiggles");
+greet("Denise", new Date());
+// above, `Date()` won't work for a second argument because Date() returns a string; however, new Date() will return an instance of the Date type, which is what we want
+// testing --noImplicitAny
+function noImplicitAny(param) {
+    return param;
+}
--- hello.ts
+++ hello.ts
@@ -1,11 +1,12 @@
-console.log("Hello world!")
-
 // the following lines for `greet` generated an error when compiling but `tsc` still emitted code; to prevent emitting when there's an error, use the flag `--noEmitOnError` before the file name when using `tsc`
-function greet(person, date) {
+function greet(person: string, date: Date) {
     console.log(`Hello ${person}, today is ${date}`)
 }
 
-greet("Wiggles")
+greet("Denise", new Date());
+// above, `Date()` won't work for a second argument because Date() returns a string; however, new Date() will return an instance of the Date type, which is what we want
 
-
-greet("Wiggles", 1/1/11)
+// testing --noImplicitAny
+function noImplicitAny(param) {
+    return param;
+}
